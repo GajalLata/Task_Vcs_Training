2d75b73803c4278c9c05e4c3fbca5d25d8105e3e
GAJAL LATA <71719656+GajalLata@users.noreply.github.com>
2022-10-10 23:14:20 +0530

    Create Calculations.java

---
+++ Calculations.java
@@ -0,0 +1,16 @@
+public class Calculations{
+  static int add(int a,int b)
+  {
+    return a+b;
+  }
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(Sytsem.in);
+     System.out.println("Enter your num1");
+    int num1=sc.nextIn();
+     System.out.println("Enter your num2");
+     int num2=sc.nextIn();
+     System.out.println("Addition: "+add(a,b));
+  }
+  }
+  
